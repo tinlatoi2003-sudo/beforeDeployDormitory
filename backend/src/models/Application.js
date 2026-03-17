0d7e6b5463997098bb66a7e8ae388f070e670534
miten202 <mtiendz202@gmail.com>
2026-03-17 21:37:17 +0700

    up Application

--- backend/src/models/Application.js
+++ backend/src/models/Application.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-
+//,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+// Application model: đại diện cho đơn đăng ký, hủy, chuyển phòng của sinh viên
 const applicationSchema = new mongoose.Schema(
     {
         student: {
